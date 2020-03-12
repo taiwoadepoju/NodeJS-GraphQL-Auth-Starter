bf0b3ba0c42606e11b40423d25d516231478fa52
Taiwo Adepoju <adepojutaiwoandrew@gmail.com>
2020-03-12 19:46:17 +0100

    Added global project variables

---
+++ config/constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+  SENDGRID_API_KEY: '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',
+  BASE_URL: process.env.BASE_URL,
+  BEARER_TOKEN: 'Authorization',
+  JWT_KEY: process.env.JWT_PRIVATE_KEY,
+  JWT_EXPIRY: '1h',
+}
